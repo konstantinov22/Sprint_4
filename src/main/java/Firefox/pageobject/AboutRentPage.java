7522e83dff4778a327b72d849a6161e1d292e0a1
Maxim Konstantinov <mk22041995@yandex.ru>
2024-12-10 20:50:55 +0300

    Firefox

---
+++ src/main/java/Firefox/pageobject/AboutRentPage.java
@@ -0,0 +1,60 @@
+package Firefox.pageobject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertTrue;
+
+public class AboutRentPage {
+    WebDriver webDriver;
+
+    public AboutRentPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    private By dateInput = By.xpath(".//input[@placeholder='* Когда привезти самокат']");
+    private By rentalPeriodDropdown = By.xpath(".//span[@class='Dropdown-arrow']");
+    private By commentInput = By.xpath(".//div[@class='Input_InputContainer__3NykH']/input[@class='Input_Input__1iN_Z Input_Responsible__1jDKN']");
+    private By orderButton = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
+    private By yesButton = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Да']");
+    private By OrderСonfirmWindow = By.xpath(".//*[text()='Заказ оформлен']");
+
+    public void writeDateOfDelivery(String date) {
+        webDriver.findElement(dateInput).sendKeys(date);
+    }
+
+    public void chooseRentalPeriod(String time) {
+        webDriver.findElement(rentalPeriodDropdown).click();
+        webDriver.findElement(By.xpath(String.format("//*[text()='%s']", time))).click();
+    }
+
+    public void chooseColor(String color) {
+        webDriver.findElement(By.xpath(String.format(".//*[text()='%s']", color))).click();
+    }
+
+    public void leaveComment(String comment) {
+        webDriver.findElement(commentInput).sendKeys(comment);
+    }
+
+    public void clickOnOrderButton() {
+        webDriver.findElement(orderButton).click();
+    }
+
+    public void clickOnYesButton() {
+        webDriver.findElement(yesButton).click();
+    }
+
+    public void checkOrderIsConfirm() {
+        var isDisplayed = webDriver.findElement(OrderСonfirmWindow).isDisplayed();
+        assertTrue("Заказ не зарегистрирован", isDisplayed);
+    }
+
+    public void fillSecondOrderPage(String date, String time, String color, String comment) {
+        writeDateOfDelivery(date);
+        chooseRentalPeriod(time);
+        chooseColor(color);
+        leaveComment(comment);
+        clickOnOrderButton();
+
+    }
+}
---
+++ src/main/java/Firefox/pageobject/MainPage.java
@@ -0,0 +1,50 @@
+package Firefox.pageobject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class MainPage {
+    WebDriver webDriver;
+
+    public MainPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    private By sectionOfQuestions = By.xpath(".//div[@class='accordion']");
+
+    public void hideCookieWindow() {
+        WebDriverWait wait = new WebDriverWait(webDriver, 10);
+        By cookieButtonLocator = By.xpath(".//div/button[@id='rcc-confirm-button']");
+        WebElement cookieButton = wait.until(ExpectedConditions.visibilityOfElementLocated(cookieButtonLocator));
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", cookieButton);
+        cookieButton.click();
+    }
+
+    public void scrollToSectionOfQuestions() {
+        WebElement sectionElement = webDriver.findElement(sectionOfQuestions);
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", sectionElement);
+    }
+
+    public void clickOnQuestion(int questionIndex) {
+        By questionLocator = By.xpath(".//div[@class='accordion__button' and @id='accordion__heading-" + questionIndex + "']");
+        webDriver.findElement(questionLocator).click();
+    }
+
+    public String getTextOfAnswer(int answerIndex) {
+        By answerLocator = By.xpath(".//div[@id='accordion__panel-" + answerIndex + "']");
+        return webDriver.findElement(answerLocator).getText();
+    }
+
+    public void clickOnOrderButton() {
+        WebDriverWait wait = new WebDriverWait(webDriver, 10);
+        By orderButtonLocator = By.xpath(".//div[@class='Header_Nav__AGCXC']/button[@class='Button_Button__ra12g']");
+        WebElement orderButton = wait.until(ExpectedConditions.visibilityOfElementLocated(orderButtonLocator));
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", orderButton);
+        orderButton.click();
+    }
+}
