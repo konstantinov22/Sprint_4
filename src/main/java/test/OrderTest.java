08e029fac125c0679a58ca4f7a94f064835352c6
mk220 <mk22041995@gmail.com>
2024-12-16 19:21:23 +0300

    Занес данные в параметризацию. Ольга и Максим

--- src/main/java/test/OrderTest.java
+++ src/main/java/test/OrderTest.java
@@ -22,21 +22,50 @@ import static base.url.URL.APP_URL;
 @RunWith(Parameterized.class)
 public class OrderTest {
     private WebDriver webDriver;
-    private String browser;
+    private final String browser;
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String metroStation;
+    private final String phoneNumber;
+    private final String date;
+    private final String rentalPeriod;
+    private final String scooterColor;
+    private final String comment;
+    private final boolean useTopButton;
+
     MainPage mainPage;
     ForWhomPage forWhomPage;
     AboutRentPage aboutRentPage;
 
-    @Parameterized.Parameters(name = "{index}: Browser = {0}")
+    @Parameterized.Parameters(name = "{index}: Browser={0}, FirstName={1}, LastName={2}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {"chrome"},
-                {"firefox"}
+                {"chrome", "Максим", "Константинов", "Ротмистрова 27", "Спортивная", "+79209999999",
+                        "08.12.2024", "сутки", "серая безысходность", "Жду самокат", true},
+                {"chrome", "Ольга", "Хрусталева", "Калинина 94", "Черкизовская", "+79177777777",
+                        "15.12.2024", "двое суток", "чёрный жемчуг", "Пишите когда будете подьезжать", true},
+                {"firefox", "Максим", "Константинов", "Ротмистрова 27", "Спортивная", "+79209999999",
+                        "08.12.2024", "сутки", "серая безысходность", "Жду самокат", false},
+                {"firefox", "Ольга", "Хрусталева", "Калинина 94", "Черкизовская", "+79177777777",
+                        "15.12.2024", "двое суток", "чёрный жемчуг", "Пишите когда будете подьезжать", false}
         });
     }
 
-    public OrderTest(String browser) {
+    public OrderTest(String browser, String firstName, String lastName, String address, String metroStation,
+                     String phoneNumber, String date, String rentalPeriod, String scooterColor,
+                     String comment, boolean useTopButton) {
         this.browser = browser;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.metroStation = metroStation;
+        this.phoneNumber = phoneNumber;
+        this.date = date;
+        this.rentalPeriod = rentalPeriod;
+        this.scooterColor = scooterColor;
+        this.comment = comment;
+        this.useTopButton = useTopButton;
     }
 
     @Before
@@ -52,20 +81,7 @@ public class OrderTest {
     }
 
     @Test
-    public void checkOrderOfScooterMaximTest() {
-        executeOrderTest("Максим", "Константинов", "Ротмистрова 27", "Спортивная", "+79209999999",
-                "08.12.2024", "сутки", "серая безысходность", "Жду самокат", true);
-    }
-
-    @Test
-    public void checkOrderOfScooterOlgaTest() {
-        executeOrderTest("Ольга", "Хрусталева", "Калинина 94", "Черкизовская", "+79177777777",
-                "15.12.2024", "двое суток", "чёрный жемчуг", "Пишите когда будете подьезжать", false);
-    }
-
-    private void executeOrderTest(String firstName, String lastName, String address, String metroStation,
-                                  String phoneNumber, String date, String rentalPeriod, String scooterColor,
-                                  String comment, boolean useTopButton) {
+    public void checkOrderTest() {
         mainPage = new MainPage(webDriver);
 
         webDriver.get(APP_URL);
